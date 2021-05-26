b78d3a4daf69e2c1cb1cf8b1352c7036a78b406b
Carmela Leung <54586536+carmelal@users.noreply.github.com>
2021-05-26 10:14:09 -0400

    Run VimPlug when setting up

--- setup.sh
+++ setup.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+# Run VimPlug
+echo "Running VimPlug..."
+vim +VimPlug +qa!
+
 ## FOR SPIN ENVIRONMENT ##
 if [ $SPIN ]; then
   # move files from ~/dotfiles
+  echo "Moving files..."
   ln -sf ~/dotfiles/.vimrc ~/.vimrc
   ln -sf ~/dotfiles/.vim ~/.vim
   ln -sf ~/dotfiles/.zshrc ~/.zshrc
-  
+
   # install packages
   if ! command -v fzf &> /dev/null; then
+    echo "Installing FZF..."
     sudo apt-get install -y fzf
   fi
 fi
