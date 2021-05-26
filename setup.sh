7a169c8b9564039b3bbf4a80f4f20e9343adc169
Carmela Leung <54586536+carmelal@users.noreply.github.com>
2021-05-25 22:05:13 -0400

    sigh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,14 @@
 
 ## FOR SPIN ENVIRONMENT ##
 if [ $SPIN ]; then
-  {{
-    # move files from ~/dotfiles
-    ln -sf ~/dotfiles/.vimrc ~/.vimrc
-    ln -sf ~/dotfiles/.vim ~/.vim
-    ~/dotfiles.zshrc >> ~/.zshrc
-  } && {
-  # install packages if moving works
+  # move files from ~/dotfiles
+  ln -sf ~/dotfiles/.vimrc ~/.vimrc
+  ln -sf ~/dotfiles/.vim ~/.vim
+  ~/dotfiles.zshrc >> ~/.zshrc
+  
+  # install packages
   if ! command -v fzf &> /dev/null; then
     sudo apt-get install -y fzf
   fi
-  }} || {
-  echo "Setup not work :("
-  }
 fi
 
