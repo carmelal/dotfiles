c93791aa791661cb5862f5cd2edc0f85812f9317
Carmela Leung <54586536+carmelal@users.noreply.github.com>
2021-05-25 20:16:24 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ if [ $SPIN ]; then
   # move files from ~/dotfiles
   ln -sf ~/dotfiles/.vimrc ~/.vimrc
   ln -sf ~/dotfiles/.zshrc ~/.zshrc
-  ln -sf ~/dotfiles/vim ~/.vim
+  ln -sf ~/dotfiles/.vim ~/.vim
 
   # install packages
   if ! command -v fzf &> /dev/null; then
